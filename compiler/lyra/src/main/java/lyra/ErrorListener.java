6a9856542a0ab486ac52ab11daf245ac4cf40c01
Alexis Armin Huf <alexishuf@users.noreply.github.com>
2015-04-25 15:58:29 -0300

    ErrorListener com mensages de erro melhoradas. --verbose-errors mostra a pilha de regras do parser no momento do erro. -- gui-error-context mostra um JDialog com a árvore do contexto com erro para cada erro de sintaxe.

---
+++ compiler/lyra/src/main/java/lyra/ErrorListener.java
@@ -0,0 +1,69 @@
+package lyra;
+
+import lyra.LyraParser;
+import org.antlr.v4.runtime.*;
+
+import javax.swing.*;
+import javax.swing.text.html.parser.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Future;
+
+/**
+ * Outputs syntatic error messages with added verbose information.
+ *
+ * For now, the extra is just the parser rule stack at the point of error.
+ */
+public class ErrorListener extends BaseErrorListener {
+    private boolean verbose;
+
+    private List<ParserRuleContext> errorContexts = new LinkedList<>();
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
+                            String msg, RecognitionException e) {
+        LyraParser parser = (LyraParser)recognizer;
+
+        String offendingSymbolString = getOffendingSymbolString(offendingSymbol, parser);
+        System.err.println(String.format("Error: line %1$d:%2$d at %3$s: %4$s", line, charPositionInLine,
+                                         offendingSymbolString, msg));
+
+        errorContexts.add(parser.getContext());
+
+        if (isVerbose()) {
+            List<String> stack = parser.getRuleInvocationStack();
+            Collections.reverse(stack);
+            System.err.println("Parser rule stack: " + stack);
+        }
+    }
+
+    private String getOffendingSymbolString(Object offendingSymbol, LyraParser parser) {
+        String offendingSymbolString = offendingSymbol.toString();
+        if (offendingSymbol instanceof Token) {
+            Token token = ((Token) offendingSymbol);
+            String tokenText = token.getText();
+            String tokenTypePart = parser.getVocabulary().getDisplayName(token.getType());
+            if (tokenTypePart.equals(String.format("'%1$s'", tokenText)))
+                tokenTypePart = "";
+            else
+                tokenTypePart = " (" + tokenTypePart + ")";
+            offendingSymbolString = String.format("\"%1$s\"%2$s", tokenText, tokenTypePart);
+
+        }
+        return offendingSymbolString;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public List<ParserRuleContext> getErrorContexts() {
+        return errorContexts;
+    }
+}
--- compiler/lyra/src/main/java/lyra/Frontend.java
+++ compiler/lyra/src/main/java/lyra/Frontend.java
@@ -7,8 +7,10 @@ import lyra.LyraLexer;
 import lyra.LyraParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.kohsuke.args4j.*;
 
+import javax.swing.text.html.parser.Parser;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +24,14 @@ public class Frontend {
     @Option(name = "--lisp", aliases = {"-l"}, required = false,
             usage = "Show LISP-style parse tree.")
     private boolean showLispTree = false;
+
+    @Option(name = "--verbose-errors", aliases = {"-E"}, required = false,
+            usage = "Print more information with error messages")
+    private boolean verboseErrors = false;
+
+    @Option(name = "--gui-error-context", aliases = {"-G"}, required = false,
+            usage = "Show a dialog with a tree representation of the parser context for each error.")
+    private boolean guiErrorContext = false;
 
     @Argument
     private List<String> files = new ArrayList<>();
@@ -62,12 +72,29 @@ public class Frontend {
         ANTLRInputStream antlrIn = new ANTLRInputStream(input);
         LyraLexer lexer = new LyraLexer(antlrIn);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+
         LyraParser parser = new LyraParser(tokens);
+        parser.removeErrorListeners();
+
+        ErrorListener errorListener = new ErrorListener();
+        if (verboseErrors)
+            errorListener.setVerbose(true);
+        parser.addErrorListener(errorListener);
+
+        //parse
         LyraParser.ProgramContext tree = parser.program();
 
-        if (showLispTree) System.out.println(tree.toStringTree(parser));
+        if (guiErrorContext) {
+            for (ParserRuleContext ctxt : errorListener.getErrorContexts()) {
+                ctxt.inspect(parser);
+            }
+        }
 
-        if (showTreeDialog) tree.inspect(parser);
+        if (showLispTree)
+            System.out.println(tree.toStringTree(parser));
+
+        if (showTreeDialog)
+            tree.inspect(parser);
 
         return parser.getNumberOfSyntaxErrors() == 0;
     }
