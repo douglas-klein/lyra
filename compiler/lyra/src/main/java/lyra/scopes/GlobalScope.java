bc3e13e6c89608780b56b5077310efb8f24919de
Alexis Armin Huf <alexishuf@users.noreply.github.com>
2015-05-04 19:49:23 -0300

    Merge branch 'master' of github.com:douglasklein2/ant into lyra_compiler
    # Conflicts:
    #	compiler/lyra/src/main/java/lyra/CommandlineUI.java
    #	compiler/lyra/src/main/java/lyra/Frontend.java
    #	compiler/lyra/src/main/java/lyra/other/Frontend.java
    #	compiler/lyra/src/test/java/lyra/FrontendTest.java

--- compiler/lyra/src/main/java/lyra/scopes/GlobalScope.java
+++
@@ -1,9 +0,0 @@
-package lyra.scopes;
-
-/**
- * Created by eduardo on 29/04/15.
- */
-public class GlobalScope extends BaseScope {
-    public GlobalScope() { super(null); }
-    public String getScopeName() { return "global"; }
-}
--- compiler/lyra/src/main/java/lyra/scopes/LocalScope.java
+++
@@ -1,9 +0,0 @@
-package lyra.scopes;
-
-/**
- * Created by eduardo on 29/04/15.
- */
-public class LocalScope extends BaseScope {
-    public LocalScope(Scope parent) { super(parent); }
-    public String getScopeName() { return "local"; }
-}
