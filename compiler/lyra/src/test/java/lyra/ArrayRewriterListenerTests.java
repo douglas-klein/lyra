485c6c304a51b073e705ffb688cb70682837c1d6
Alexis Armin Huf <alexishuf@users.noreply.github.com>
2015-06-04 21:11:56 -0300

    Testes para reescrita de acesso a array de duas dimensões.

--- compiler/lyra/src/test/java/lyra/ArrayRewriterListenerTests.java
+++ compiler/lyra/src/test/java/lyra/ArrayRewriterListenerTests.java
@@ -1,6 +1,6 @@
 package lyra;
 
-import junit.framework.Assert;
+import lyra.LyraParser;
 import lyra.listeners.ArrayRewriterListener;
 import lyra.listeners.SyntacticSugarListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
@@ -10,9 +10,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for ArrayRewriterListener
@@ -142,6 +142,66 @@ public class ArrayRewriterListenerTests {
                 assertEquals(2, args.expr().size());
                 assertTrue(Pattern.matches("\\(?0\\)?", args.expr(0).getText()));
                 assertTrue(Pattern.matches("\\(?4640\\)?", args.expr(1).getText()));
+            }
+        }, compiler.getParseTree());
+
+        assertTrue(visited[0]);
+        assertTrue(visited[1]);
+    }
+
+    @Test
+    public void testRewrite2DArrayAccess() throws Exception {
+        Compiler compiler = new Compiler();
+        compiler.init(getReader("samples/Rewrite2DArrayAccess.ly"));
+        assertTrue(compiler.parse());
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(new SyntacticSugarListener(), compiler.getParseTree());
+        walker.walk(new ArrayRewriterListener(), compiler.getParseTree());
+
+        final boolean visited[] = {false, false};
+        walker.walk(new lyra.LyraParserBaseListener() {
+            @Override
+            public void exitVarDecl(lyra.LyraParser.VarDeclContext ctx) {
+                if (!ctx.varDeclUnit(0).IDENT().getText().equals("x"))
+                    return;
+
+                visited[0] = true;
+                assertNotNull(ctx.varDeclUnit(0).expr());
+                LyraParser.FactorContext factor_ = ctx.varDeclUnit(0).expr().unaryexpr().factor();
+                assertTrue(factor_ instanceof LyraParser.MemberFactorContext);
+                LyraParser.MemberFactorContext factor = (LyraParser.MemberFactorContext) factor_;
+
+                assertEquals("__at", factor.IDENT().getText());
+                assertTrue(Pattern.matches("\\(?2\\)?", factor.args().getText()));
+
+                assertTrue(factor.factor() instanceof LyraParser.MemberFactorContext);
+                LyraParser.MemberFactorContext leftFactor =
+                        (LyraParser.MemberFactorContext) factor.factor();
+                assertEquals("__at", leftFactor.IDENT().getText());
+                assertTrue(Pattern.matches("\\(?1\\)?", leftFactor.args().getText()));
+            }
+
+            @Override
+            public void exitExpr(LyraParser.ExprContext ctx) {
+                if (ctx.getText().indexOf("4640") < 0) return;
+                if (ctx.unaryexpr() == null) return;
+                if (ctx.unaryexpr().factor() == null) return;
+                if (!(ctx.unaryexpr().factor() instanceof LyraParser.MemberFactorContext)) return;
+
+                visited[1] = true;
+                LyraParser.MemberFactorContext set
+                        = (LyraParser.MemberFactorContext) ctx.unaryexpr().factor();
+
+                assertEquals("__set", set.IDENT().getText());
+                assertTrue(Pattern.matches("\\(*1\\)*", set.args().expr(0).getText()));
+                assertTrue(Pattern.matches("\\(*4640\\)*", set.args().expr(1).getText()));
+
+                assertTrue(set.factor() instanceof LyraParser.MemberFactorContext);
+                LyraParser.MemberFactorContext at = (LyraParser.MemberFactorContext) set.factor();
+                assertEquals("__at", at.IDENT().getText());
+                assertTrue(Pattern.matches("\\(*0\\)*", at.args().expr(0).getText()));
+                assertTrue(Pattern.matches("\\(*arr\\)*", at.factor().getText()));
             }
         }, compiler.getParseTree());
 
---
+++ compiler/lyra/src/test/resources/samples/Rewrite2DArrayAccess.ly
@@ -0,0 +1,7 @@
+class Application {
+    def main {
+        Int[][] arr = new Int[2][3];
+        arr[0][1] = 4640;
+        Int x = arr[1][2];
+    }
+}
